51f53bdfeb29c062df0d63143703bf295510d285
Guz117 <cristian.guzman117@gmail.com>
2021-12-16 16:27:53 +0100

    bonus 2 primo tentativo

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,5 +1,8 @@
 /*
-Partendo dal markup allegato, rifare lo slider ma questa volta usando Vue.
+Bonus:
+1- al click su uno dei pallini mostrare l’immagine in posizione corrispondente
+2- applicare l’autoplay allo slider: ogni 3 secondi, 
+   cambia immagine automaticamente
 */
 
 var app = new Vue({
@@ -14,18 +17,27 @@ var app = new Vue({
       ]
     },
     methods: {
-        next: function () {
+        // next: function () {
+        //     this.counter += 1;
+        //     if (this.counter > this.images.length - 1) {
+        //         this.counter = 0;
+        //     }
+        // },
+        // prev: function () {
+        //     this.counter -= 1;
+        //     if (this.counter < 0) {
+        //         this.counter = this.images.length -1;
+        //     }
+        // },
+        image: setInterval (function()  {
             this.counter += 1;
             if (this.counter > this.images.length - 1) {
                 this.counter = 0;
             }
-        },
-        prev: function () {
-            this.counter -= 1;
-            if (this.counter < 0) {
-                this.counter = this.images.length -1;
-            }
-        },
+        }, 3000)
+    },
+    created() {
+        
     }
   })
 
