ee3f7087983194376429e79f835ea5bd21355f6f
Guz117 <cristian.guzman117@gmail.com>
2021-12-16 15:26:07 +0100

    change img

--- js/script.js
+++ js/script.js
@@ -5,6 +5,28 @@ Partendo dal markup allegato, rifare lo slider ma questa volta usando Vue.
 var app = new Vue({
     el: '#app',
     data: {
-      
+      counter: 0,
+      images: [
+          'image1.jpg',
+          'image2.jpg',
+          'image3.jpg',
+          'image4.jpg',
+      ]
+    },
+    methods: {
+        next: function () {
+            this.counter += 1;
+            if (this.counter > this.images.length - 1) {
+                this.counter = 0;
+            }
+        },
+        prev: function () {
+            this.counter -= 1;
+            if (this.counter < 0) {
+                this.counter = this.images.length -1;
+            }
+        },
     }
   })
+
+
